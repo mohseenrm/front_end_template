19f0b472a31504fc179347537ce08b409fd3e902
mohseenrm <mohseenmukaddam6@gmail.com>
2017-01-20 11:05:24 -0700

    adding updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//need to add sass compiling
+// need to add sass compiling
 const gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
